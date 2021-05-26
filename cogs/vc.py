3468bee4addf6e4789a24ea6f4c5da56b4941d46
himanshudas75 <himanshudas75@gmail.com>
2021-05-26 17:07:13 +0530

    l

--- cogs/vc.py
+++ cogs/vc.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from discord import FFmpegPCMAudio
+import os
+import re
 
 class VC(commands.Cog):
 
@@ -9,21 +11,48 @@ class VC(commands.Cog):
 
     @commands.command()
     async def join(self,ctx):
-        try:
+        if ctx.author.voice:
             channel=ctx.author.voice.channel
             await channel.connect()
             await ctx.send(f'Joined VC {channel}')
-        except AttributeError:
+        else:
             await ctx.send('Join a VC first!')
     
     @commands.command()
     async def leave(self,ctx):
-        try:
+        if ctx.voice_client:
             vc=ctx.voice_client
             await vc.disconnect()
             await ctx.send(f'Disconnected from VC')
-        except AttributeError:
+        else:
             await ctx.send('I am not in a VC!')
+    
+    @commands.command()
+    async def play(self,ctx,*,args):
+        bot=self.bot
+        if not ctx.author.voice:
+            await ctx.send('Join a VC first!')
+            return
+        if not ctx.voice_client:
+            await ctx.send('I am not in a VC!')
+            return
+
+        found=False
+        for filename in os.listdir('./audios'):
+            name=re.findall('\w+',filename)[0]
+            if args.lower() == name.lower():
+                found=filename
+                break
+        if not found:
+            await ctx.send('Song not found')
+            return
+
+        source=FFmpegPCMAudio(f'./audios/{filename}')
+        voice=discord.utils.get(bot.voice_clients,guild=ctx.guild)
+        voice.play(source)
+    
+    @commands.command()
+
 			
 def setup(bot):
 	bot.add_cog(VC(bot))
