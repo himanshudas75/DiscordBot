6c6fb4ed2e395715d28e068154b0db6c78631c97
himanshudas75 <himanshudas75@gmail.com>
2021-07-12 22:02:30 +0530

    s

--- bot_commands/nasa.py
+++ bot_commands/nasa.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import datetime
 from dotenv import load_dotenv
 
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -12,9 +13,15 @@ def apod(date):
     if date=="":
         url=f'https://api.nasa.gov/planetary/apod?count=1&api_key={NASA_KEY}'
     else:
-        url=f'https://api.nasa.gov/planetary/apod?count=1&date={date}&api_key={NASA_KEY}'
+        if date=="today":
+            date=datetime.date.today()
+        url=f'https://api.nasa.gov/planetary/apod?date={date}&api_key={NASA_KEY}'
     r=requests.get(url)
-    json_data=json.loads(r.text)[0]
+    json_data=json.loads(r.text)
+
+    if isinstance(json_data,list):
+        json_data=json_data[0]
+
     toret={
         "date": json_data['date'],
         "explanation": json_data['explanation'],
